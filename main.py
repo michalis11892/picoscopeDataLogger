493f0be4b7ea80983fe7976c33336aac32016a81
michael11892 <m.panayiotou11892@gmail.com>
2021-07-14 14:39:00 +0300

    Added sigGen_stndrd() & Added logical trigger to data_rapid_block()

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def data_block(chandle, status, channel, coupling, crange, offset,
     return results
 
 def data_rapid_block(chandle, status, channel, coupling, crange, offset,
-                trig_adc_counts, trig_direction, trig_delay, trig_auto, preTriggerSamples, postTriggerSamples,
+                simpleTrig, trig_conditions, trig_adc_counts, trig_direction, trig_delay, trig_auto, preTriggerSamples, postTriggerSamples,
                 timebase, segments, captures, downsampling_ratio_mode, downsampling_ratio):
     '''
     chandle -> Picoscope handle
@@ -210,6 +210,19 @@ def data_rapid_block(chandle, status, channel, coupling, crange, offset,
         MAX RANGE  -> 12
     offset:
         Any offset, same units as range
+    simpleTrig:
+        No conditions -> True
+        Logical conditions (AND, OR, etc...) -> False
+    trig_conditions:
+        List of lists, [list1, list2, list3, ...]
+        where each list represents the parameters of a condition structure
+        list1 = [channelA; channelB; channelC; channelD; external; aux; pulseWidthQualifier; digital]
+        where channelA/B/C/D/etc:
+            Don't care -> 0
+            True -> 1
+            False -> 2
+        such that all parameters within a condition structure are ANDed (channelA AND channelB)
+        and all condition structres are then ORed (struct_list1 OR struct_list2 OR struct_list3 OR ...)
     trig_adc_counts:
         -32512 <= trig_adc_counts <= +32512 (trigger threshold),
             -32512  -> corresponds to the Minimum Range Voltage
@@ -256,7 +269,21 @@ def data_rapid_block(chandle, status, channel, coupling, crange, offset,
         status["trigger"] = ps.ps2000aSetSimpleTrigger(chandle, 1, 1, trig_adc_counts, trig_direction, trig_delay, trig_auto)
         assert_pico_ok(status["trigger"])
 
+    #Check for logical triggering conditions
+    if not simpleTrig:
+        cond_list = []
+        try:
+            for condition in trig_conditions:
+                cond_list.append(PS2000A_TRIGGER_CONDITIONS(condition[0], condition[1], condition[2], condition[3], condition[4], condition[5], condition[6], condition[7]))
 
+            command = 'ps2000a.ps2000aSetTriggerChannelConditions(chandle'
+            for i in range(len(cond_list)):
+                command += ', cond_list['+str(i)+']'
+            command += ', '+str(len(cond_list))+')'
+            exec(comand) #BE VARY CAREFUL!!! VERY HARMFUL IF EXPLOITED
+        except:
+            print('ERROR')
+            exit()
 
     # Setting the number of sample to be collected
     maxsamples = preTriggerSamples + postTriggerSamples
@@ -497,6 +524,93 @@ def data_streaming(chandle, status, channel, coupling, crange, offset,
 
     return results
 
+def sigGen_stndrd(chandle, status, offsetVoltage, pkToPk, waveType,  startFrequency, stopFrequency, increment, dwellTime, sweepType, operation, shots, sweeps, triggerType, triggerSource, extInThreshold):
+    '''
+    chandle -> Picoscope handle
+    status:
+        Empty list
+    offsetVoltage:
+        Signal offset voltage from 0, in μV
+    pkToPk:
+        Signal peak-to-peak voltage, in μV
+    waveType:
+        Sine -> 0
+        Square -> 1
+        Triangle -> 2
+        DC Voltage -> 3
+        Rising Sawtooth (Ramp Up) -> 4
+        Falling Sawtooth (Ramp Down) -> 5
+        Sin(x)/x -> 6
+        Gaussian -> 7
+        Half Sine -> 8
+    startFrequency:
+        Start frequency, in Hz (careful wiht specific signal frequency bounds)
+    stopFrequency:
+        End frequency, in Hz (careful wiht specific signal frequency bounds)
+    increment:
+        Amount of frequency increase or decrease in sweep mode
+    dwellTime:
+        Time for which the sweep stays at each frequency, in seconds
+    sweepType:
+        Up -> 0
+        Down -> 1
+        Up-Down -> 2
+        Down-Up -> 3
+    operation:
+        Normal signal generator operation specified by waveType -> 0
+        White noise and ignores all settings except pkToPk and offsetVoltage -> 1
+        Pseudorandom binary sequence at the specified frequency or frequency range -> 2
+    shots:
+        Sweep the frequency as specified by sweeps -> 0
+            OR
+        The number of cycles of the waveform to be produced after a trigger event [sweeps must be zero]
+        (not PicoScope 2205 MSO)
+    sweeps:
+        Produce number of cycles specified by shots -> 0
+            OR
+        The number of times to sweep the frequency after a trigger event, according to sweepType [shots must be zero]
+    triggerType:
+        Rising -> 0
+        Falling -> 1
+        Gate High -> 2
+        Gate Low -> 3
+    triggerSource:
+        None -> 0
+        Scope Trigger -> 1
+        Ext Input -> 2
+        Software Trigger [ps2000aSigGenSoftwareControl()] -> 3
+        Reserved -> 4
+    extInThreshold:
+        Trigger level, in ADC counts, for external trigger
+    '''
+    waveType = ctypes.c_int16(waveType)
+    sweepType = ctypes.c_int32(sweepType)
+    triggertype = ctypes.c_int32(triggerType)
+    triggerSource = ctypes.c_int32(triggerSource)
+
+    status["SetSigGenBuiltIn"] = ps.ps2000aSetSigGenBuiltIn(chandle, offsetVoltage, pkToPk, waveType, startFrequency, stopFrequency, increment, dwellTime, sweepType, operation, shots, sweeps, triggertype, triggerSource, extInThreshold)
+    assert_pico_ok(status["SetSigGenBuiltIn"])
+
+def restart_scope(chandle, status):
+    global chandle, status
+    # Stop the scope
+    status["stop"] = ps.ps2000aStop(chandle)
+    assert_pico_ok(status["stop"])
+
+    # Close unitDisconnect the scope
+    status["close"] = ps.ps2000aCloseUnit(chandle)
+    assert_pico_ok(status["close"])
+
+    # display status returns
+    print(status)
+
+    chandle = ctypes.c_int16()
+    status = {}
+
+    # Open the scope
+    status["openunit"] = ps.ps2000aOpenUnit(ctypes.byref(chandle), None)
+    assert_pico_ok(status["openunit"])
+
 #-----------------------------------------[Main Code]-----------------------------------------
 chandle = ctypes.c_int16()
 status = {}
@@ -513,15 +627,15 @@ for axis in data_block(chandle, status, 'A', [0,0], [6,0], [0,0], 'A', 1024, 2, 
         f.write(str(element)+'\n')
 f.close()'''
 
-'''#TEST data_rapid_block()
+#TEST data_rapid_block()
 f = open('test.txt', 'w')
-for list in data_rapid_block(chandle, status, 'A', 0, 6, 0, 1024, 2, 0, 1000, 800, 800, 32, 10, 10, 0, 1):
+for list in data_rapid_block(chandle, status, 'A', 0, 6, 0, False, [], 1024, 2, 0, 1000, 800, 800, 32, 10, 10, 0, 1):
     f.write('--------------------------------------------------------------------=[List]=--------------------------------------------------------------------\n')
     for axis in list:
         f.write('--------------------------------------------------------------------=[Axis]=--------------------------------------------------------------------\n')
         for element in axis:
             f.write(str(element)+'\n')
-f.close()'''
+f.close()
 
 '''#TEST data_streaming()
 nextSample = 0 #global variable
